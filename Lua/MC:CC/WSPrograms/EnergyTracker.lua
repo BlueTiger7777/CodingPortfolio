4fe533e79906d91f03a9a3271b545722bce861d9
BlueTiger7777 <73462829+BlueTiger7777@users.noreply.github.com>
2025-04-18 10:39:52 +0100

    Update EnergyTracker.lua

--- Lua/MC:CC/WSPrograms/EnergyTracker.lua
+++ Lua/MC:CC/WSPrograms/EnergyTracker.lua
@@ -3,9 +3,10 @@ local ws = http.websocket("0.0.0.0:9001")
 local bat = peripheral.wrap("back")
 local ME = bat.getMaxEnergy() * 0.4
 local CE = bat.getEnergy() * 0.4
+local TC = bat.getTransferCap() * 0.4
 local EI = bat.getLastInput() * 0.4
 local EO = bat.getLastOutput() * 0.4
-local msg = ME.." "..CE.." "..EI.." "..EO
+local msg = ME.." "..CE.." "..TC.." "..EI.." "..EO
 
 -- Tells the websocket that this client is a tracker
 ws.send("Tracker")
@@ -16,7 +17,8 @@ while true do
   os.sleep(5)
   ME = bat.getMaxEnergy() * 0.4
   CE = bat.getEnergy() * 0.4
+  TC = bat.getTransferCap() * 0.4
   EI = bat.getLastInput() * 0.4
   EO = bat.getLastOutput() * 0.4
-  msg = ME.." "..CE.." "..EI.." "..EO
+  msg = ME.." "..CE.." "..TC.." "..EI.." "..EO
 end
