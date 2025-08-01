8edf5c8ac9679413fc99cf803321993a4f11938e
BlueTiger7777 <73462829+BlueTiger7777@users.noreply.github.com>
2025-08-01 18:19:45 +0100

    Update startup.lua

--- Lua/MC:CC/MusicPlayer/startup.lua
+++ Lua/MC:CC/MusicPlayer/startup.lua
@@ -1,5 +1,5 @@
 -- Defineing Notes (asumes first 8 redstone intergrators are on this network and create is installed)
-local function MP_HFs do
+local function MP_HFs() do
   local relay = peripheral.wrap("redstone_relay_0")
   local side = "front"
   if relay.getOutput(side) then
